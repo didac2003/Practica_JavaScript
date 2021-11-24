162db88805aad627ba64361d946a634305393715
didac2003 <94047560+didac2003@users.noreply.github.com>
2021-11-24 11:43:41 +0100

    Add files via upload

---
+++ practica2.js
@@ -0,0 +1,98 @@
+    //Ex1
+/*
+function nom (nom, cognom) {
+    console.log(nom + cognom)
+}
+nom ('Dídac', ' Gros')
+*/
+
+//Ex2
+/*
+function fnom (num= 20) {
+    console.log(typeof num)
+}
+fnom()
+*/
+
+//Ex3
+/*
+function fdata (fecha= new Date()){
+console.log(fecha)
+}
+fdata()
+*/
+
+//Ex4
+/*
+function fdata (){
+    var variable = 65
+}
+fdata()
+console.log(variable)
+*/
+
+//Ex5
+/*
+function vocals(str) { 
+    const count = str.match(/[aeiou]/gi).length;
+    return count;
+}
+var variable ='Paraula'
+var total = vocals(variable);
+console.log(total);
+*/
+
+//Ex6
+/*
+function numeros(a,b, c=7){
+    return a+b+c
+}
+console.log(numeros(10, 6))
+*/
+
+//Ex7
+/*
+function ex7(a,b,c){
+    fdata()
+    return numeros(a,b,c)
+}
+console.log(ex7(4,5,6))
+*/
+
+//Ex8
+/*
+function numero(a,b){
+    if (a == 30 || b == 30 || a+b == 30) {
+        return true
+    }
+}
+console.log(numero(15,15))
+*/
+
+//Ex9
+/*
+var numero = function(a,b){
+if (a == 30 || b == 30 || a+b == 30) {
+    return true
+}
+}
+console.log(numero(30,15))
+*/
+
+//Ex10
+/*
+var numero = (a,b) => {
+    if (a===b)
+    return true
+    else return false
+}
+console.log(numero(3,3))
+*/
+
+//Ex11
+
+var ex11 = a => Math.sqrt(a) 
+console.log(ex11(4))
+
+
+
