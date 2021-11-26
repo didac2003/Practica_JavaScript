c9be5ed9570892b5d84dc64f709a7fe64b0c9eab
didac2003 <94047560+didac2003@users.noreply.github.com>
2021-11-26 14:28:35 +0100

    Add files via upload

---
+++ Logica/logica.js
@@ -0,0 +1,15 @@
+//Ex1
+
+var num = 21
+if (( num % 3 ) == 0){
+    console.log(num + ' es multiple de 3')
+}else if ((num % 7) == 0){
+    console.log(num + ' es multiple de 7')
+}
+else{
+    console.log('El numero no es multiple de 3 ni de 7');
+}
+
+
+
+
