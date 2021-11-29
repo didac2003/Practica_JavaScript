8bed18e38586eb8ac791ed09ec04f958d7e25f21
didac2003 <94047560+didac2003@users.noreply.github.com>
2021-11-29 08:32:54 +0100

    Add files via upload

---
+++ Logica/logica.js
@@ -0,0 +1,37 @@
+//Ex1
+/*
+var num = 21
+if (( num % 3 ) == 0){
+    console.log(num + ' es multiple de 3')
+}else if ((num % 7) == 0){
+    console.log(num + ' es multiple de 7')
+}
+else{
+    console.log('El numero no es multiple de 3 ni de 7');
+}
+*/
+
+//Ex2
+/*
+var x = 25
+var y = 145
+ 
+if (x > 10 && x <= 50 )
+console.log("El numero " + x + " esta dins el rang")
+
+if (y >10 && y <= 50)
+    console.log('El numero ' + y + ' esta dins el rang')
+
+if (x < 10 || x > 50) 
+console.log('El número ' + x + " no està dins del rang")
+
+if (y < 10 || y > 50) 
+console.log('El número ' + y + " no està dins del rang")
+*/
+
+//Ex3
+
+
+
+
+}
