148fbbb7e95b367dbe941b990dd8237577299e43
didac2003 <94047560+didac2003@users.noreply.github.com>
2021-11-29 09:45:58 +0100

    Add files via upload

---
+++ Logica/logica.js
@@ -0,0 +1,54 @@
+//Ex1
+/*
+var num = 21
+if (( num % 3 ) == 0){
+    console.log(num + ' es multiple de 3')
+}else if ((num % 7) == 0){
+    console.log(num + ' es multiple de 7')
+}
+else{
+    console.log('El numero no es multiple de 3 ni de 7');
+}
+/*
+
+//Ex2
+/*
+var x = 30
+var y = 25
+
+x=(x>=10 && x<=50) ? console.log("El número " + x + " está en el rang")
+                   : console.log("El número " + x + " no está en el rang")
+
+y=(y>=10 && y<=50) ? console.log("El número " + y + " está en el rang")
+                   : console.log("El número " + y + " no está en el rang")
+*/
+
+//Ex3
+/*
+var a = 9
+
+switch ((a<5) ? 0 : (a>=5 && a<6) ? 1 : (a>=6 && a<7) ? 2 : (a>=7 && a<9) ? 3 : 4){
+    case 0:
+        console.log("Molt bé senyor, has suspès")
+    break
+
+    case 1:
+        console.log("Molt bé senyor, un suficient")
+    break
+
+    case 2:
+        console.log("Molt bé senyor, un bé")
+        break
+
+    case 3:
+        console.log("Molt bé senyor, un notable")
+    break
+
+    case 4:
+        console.log("Molt bé senyor, un excel·lent")
+    break
+}
+*/
+
+//Ex4
+
